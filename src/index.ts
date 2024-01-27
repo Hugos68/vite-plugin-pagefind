d195058b63899e2c6596cc7a8e76ded23fb9872b
hugos68 <hugokorteapple@gmail.com>
2024-01-27 21:40:12 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { existsSync } from 'fs';
+import { execSync } from 'child_process';
+import { green } from 'colorette';
+
+function log(input: string) {
+	console.log(`${green('[vite-plugin-pagefind]')} ${input}`);
+}
+
+type PagefindConfig = {
+	buildDir: string;
+	pagefindDir: string;
+};
+
+export default function ({ buildDir, pagefindDir }: PagefindConfig) {
+	const __dirname = dirname(fileURLToPath(import.meta.url));
+
+	buildDir = join(__dirname, 'build');
+	pagefindDir = join(__dirname, pagefindDir);
+
+	function configureServer() {
+		if (!existsSync(pagefindDir)) {
+			log('Pagefind not found.');
+			if (!existsSync(join(buildDir, 'pagefind'))) {
+				log('Build not found, building...');
+				execSync('vite build');
+				log('Build complete.');
+			}
+			log('Running pagefind...');
+			execSync(
+				`pagefind --site ${buildDir} --output-path ${pagefindDir}`
+			);
+			log('Pagefind complete.');
+		}
+	}
+
+	function closeBundle() {
+		log('Running pagefind...');
+		execSync(`pagefind --site ${buildDir}`);
+		log('Pagefind complete.');
+	}
+
+	return {
+		name: 'pagefind',
+		configureServer,
+		closeBundle
+	};
+}
